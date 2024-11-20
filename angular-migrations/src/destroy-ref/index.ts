444babe0208cc53d90129cdcfee80e69a9a34fdb
AndreySkripachev <andrey-skripachev@saritasa.com>
2024-11-20 11:18:37 +0700

    Add dynamic property name calculation

--- angular-migrations/src/destroy-ref/index.ts
+++ angular-migrations/src/destroy-ref/index.ts
@@ -8,6 +8,15 @@ const FILE_REGEX = /^\/projects\/.*\.ts$/;
 const DESTROYABLE_ALIAS = 'DestroyableComponent';
 const project = new Project();
 
+interface SchematicProperties {
+
+  /** Decorator name. */
+  readonly decoratorName: string;
+
+  /** Pipe name. */
+  readonly pipeName: string;
+}
+
 function shouldProcessFile(tsSource: string): boolean {
 	return tsSource.includes(DESTROYABLE_ALIAS) || tsSource.includes('takeUntilDestroy(this)');
 }
@@ -17,24 +26,37 @@ function getComponentClasses(source: SourceFile): ClassDeclaration[] {
 		.filter(ClassUtils.isAngularFeature);
 }
 
-function processComponent(component: ClassDeclaration): void {
-	ClassUtils.removeDecorator(component, DESTROYABLE_ALIAS);
+function insertDestroyRefToComponent(component: ClassDeclaration): string {
+  let destroyRefName = 'destroyRef';
+  let existsProperty = component.getProperty(destroyRefName);
 
-	const fn = NodeUtils.findFunctionCallInNode(component, 'takeUntilDestroy');
+  while (existsProperty !== undefined) {
+    destroyRefName = `${destroyRefName}_`;
+    existsProperty = component.getProperty(destroyRefName);
+  }
+
+  component.insertProperty(0, {
+    name: 'destroyRef',
+    initializer: 'inject(DestroyRef)',
+    isReadonly: true,
+    scope: Scope.Private,
+  }).appendWhitespace('\n');
+
+  return destroyRefName;
+}
+
+function processComponent(component: ClassDeclaration, options: SchematicProperties): void {
+	ClassUtils.removeDecorator(component, options.decoratorName);
+
+	const fn = NodeUtils.findFunctionCallInNode(component, options.pipeName);
 
 	if (fn !== undefined) {
-		component.insertProperty(0, {
-			name: 'destroyRef',
-			initializer: 'inject(DestroyRef)',
-			isReadonly: true,
-			scope: Scope.Private,
-			trailingTrivia: '\n'
-		});
-		fn?.replaceWithText('takeUntilDestroyed(this.destroyRef)');
+		const propName = insertDestroyRefToComponent(component);
+		fn?.replaceWithText(`takeUntilDestroyed(this.${propName})`);
 	}
 }
 
-function processFile(tsSource: string): string {
+function processFile(tsSource: string, options: SchematicProperties): string {
 	if (!shouldProcessFile(tsSource)) {
 		return tsSource;
 	}
@@ -50,7 +72,7 @@ function processFile(tsSource: string): string {
 	ImportUtils.addImport(source, 'DestroyRef', '@angular/core', 0);
 	ImportUtils.addImport(source, 'takeUntilDestroyed', '@angular/core/rxjs-interop', 0);
 
-	components.forEach(processComponent);
+	components.forEach(component => processComponent(component, options));
 
 	ImportUtils.removeImport(source, '/destroyable', false);
 
@@ -59,14 +81,14 @@ function processFile(tsSource: string): string {
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function destroyRef(_options: any): Rule {
+export function destroyRef(options: SchematicProperties): Rule {
 	return (tree: Tree, _context: SchematicContext) => {
 		tree.visit(path => {
 			if (FILE_REGEX.test(path)) {
 				const buffer = tree.read(path);
 				if (buffer) {
 					let content = buffer.toString();
-					const proceedFile = processFile(content);
+					const proceedFile = processFile(content, options);
 					if (proceedFile !== content) {
 						tree.overwrite(path, proceedFile);
 					}
